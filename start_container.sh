6a5323019e83f4de7ac8ced62011f391eede8fb7
root <root@odbc.localdomain>
2016-09-12 17:51:51 +0200

    edit start scripts

--- start_container.sh
+++ start_container.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-
-docker run -i -p 3001:3000 \
-  -v /grafana-docker/data:/var/lib/grafana \
-  -e "GF_SERVER_ROOT_URL=http://grafana.server.name"  \
-  grafana/grafana:2.6.0
+docker run -d --name mygrafana -p 3000:3000 mygrafana
